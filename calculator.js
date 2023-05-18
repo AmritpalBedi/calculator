f77333450c12022a085f25c684b0218c12abe08f
AmritpalBedi <125513113+AmritpalBedi@users.noreply.github.com>
2023-05-17 19:13:55 -0500

    Screen, numbers, clear and delete working

--- calculator.js
+++ calculator.js
@@ -28,3 +28,49 @@ function operate(x, operator, y) {
         divide(x, y)
     }
 }
+
+const screen = document.querySelector(".nums");
+
+const digits = document.querySelectorAll(".digits");
+let num = 0;
+digits.forEach((digit) => {
+    digit.addEventListener("click", () => {
+        if (screen.textContent.length == 15) {
+            screen.textContent = screen.textContent;
+        }
+        else {
+            if (screen.textContent == "0") {
+                screen.textContent = digit.textContent;
+            }
+            else {
+                screen.textContent += digit.textContent;
+            }
+        }
+    });
+});
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+    screen.textContent = "0";
+})
+
+const del = document.querySelector(".delete");
+del.addEventListener("click", () => {
+    screen.textContent.length == 1 ? screen.textContent = 0:
+    screen.textContent = screen.textContent.slice(0, -1);
+})
+
+// const addition = document.querySelector("#add");
+// add.addEventListener("click", () => {
+//     let firstNum = +screen.textContent;
+
+// })
+
+// just using to test
+// const asdad = document.querySelectorAll(".operation");
+// asdad.forEach((digit) => {
+//     digit.addEventListener("click", () => {
+//         alert(screen.textContent.length)
+//     })
+// })
+
